5072b2f0d0576d0d8358c235c16870436472f671
Himanshu Jarodiya <himanshujar911@gmail.com>
2026-03-26 16:58:27 -0700

    remove ALB rewrite, switch to Render backend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination:
-          "http://shortlyst-alb-1378051991.us-east-1.elb.amazonaws.com/:path*",
-      },
-    ];
-  },
-};
-
-export default nextConfig;
+ 
